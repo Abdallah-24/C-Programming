0635405a882bdcc11ed1e12d9a50f669fbc25f93
Abdallah-24 <abdallaahmed241098@gmail.com>
2022-02-05 15:48:08 +0200

    first commit

---
+++ 1_C Programming Basics/LAB_1.c
@@ -0,0 +1,24 @@
+/***********************************************************/
+/************** Name 	 : Abdallah Ali-Allah **************/
+/************** Date 	 : 5/2/2022           **************/
+/************** Compiler : gcc         		  **************/
+/************** Version  : 1.0                **************/
+/**************             LAB_1             **************/
+/*   Write a C code that will print your short biography.  */
+/*   Full Name, Birth Year, Faculty, and graduation year   */
+/***********************************************************/
+
+/* Include stdio.h library to use printf function */
+#include "stdio.h"
+
+/* define the main function */
+void main(void)
+{
+	/* call the printf function and pass a string to
+		it to print */
+	printf("Hi, I'm Abdallah Ahmed Ali-Allah.\n");
+	printf("My birth date is 24 Oct 1998.\n");
+	printf("I'm an undergraduate student at faculty of Engineering Zagazig university.\n");
+
+	/* End of main function */
+}
